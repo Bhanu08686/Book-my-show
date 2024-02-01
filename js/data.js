8a6b7d72299aab78cebd3aeec70a3c2afaefe1aa
Bhanu08686 <106303910+Bhanu08686@users.noreply.github.com>
2024-02-01 10:55:22 +0530

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,20 @@
+let movies = [
+    {
+        image: 'img/banner/banner1.avif'
+    },
+    {
+        image: 'img/banner/banner2.jpg'
+    },
+    {
+        image: 'img/banner/banner3.webp'
+    },
+    {
+        image: 'img/banner/banner4.webp'
+    },
+    {
+        image: 'img/banner/banner5.webp'
+    },
+    {
+        image: 'img/banner/banner6.jpg'
+    }
+]
